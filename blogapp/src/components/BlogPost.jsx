55db124f0f856736262806ce03b8fc24b1df9660
deepak <deepak1122003kumar@gmail.com>
2023-09-29 15:34:48 +0530

    date added

--- blogapp/src/components/BlogPost.jsx
+++ blogapp/src/components/BlogPost.jsx
@@ -3,13 +3,19 @@ import PostTitle from './PostTitle';
 import PostContent from './PostContent';
 import PostAuthor from './PostAuthor';
 import BlogData from './BlogData'; 
+const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
 
 const BlogPost = () => {
+  const currentDate = new Date();
   return (
     <div>
       {BlogData.map((val, index) => ( 
         <div key={index}>
           <PostTitle title={val.title} />
+          <p>{formatDate(currentDate)}</p>
           <PostContent content={val.content} />
           <PostAuthor author={val.author} />
         </div>
