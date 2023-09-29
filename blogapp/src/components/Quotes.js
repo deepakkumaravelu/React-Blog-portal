7edadc3eb55370d5f4a77b1dc91419a8b5320862
deepak <deepak1122003kumar@gmail.com>
2023-09-29 15:49:15 +0530

    Quotes.js added

---
+++ blogapp/src/components/Quotes.js
@@ -0,0 +1,7 @@
+const Quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Life is what happens when you're busy making other plans. - John Lennon",
+    "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
+
+  ];
+  export default Quotes;
