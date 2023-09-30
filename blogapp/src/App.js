cc997f13eaa5a9979291e794b70fca8b5157d766
deepak <deepak1122003kumar@gmail.com>
2023-09-30 21:04:34 +0530

    navbar component added

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import BlogList from './components/BlogList';
-
+import NavBar from './components/NavBar';
+import './App.css';
 function App() {
   return (
+    <div>
+<NavBar/>
 <BlogList/> 
+    </div>
+
   );
 }
 
---
+++ blogapp/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+<ul className='nav'>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">News</a></li>
+    <li><a href="#">Signup</a></li>
+    <li><a href="#">Signin</a></li>
+</ul>
+    </div>
+  )
+}
+
+export default NavBar
