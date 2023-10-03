c9806288095a7b480e1c82661e7034c0e5efca06
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-03 10:21:34 +0530

    BlogPost component called in App.js

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import BlogList from './components/BlogList';
 import NavBar from './components/NavBar';
 import './App.css';
+import BlogPost from './components/BlogPost';
 function App() {
   return (
     <div>
 <NavBar/>
 <BlogList/> 
+<BlogPost/>
     </div>
 
   );
