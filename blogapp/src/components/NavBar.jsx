292009f3369a6ed1f14d4b290dfdbddfd0b2009b
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-10-04 12:00:15 +0530

    news feed added using navigate hook

--- blogapp/src/components/NavBar.jsx
+++ blogapp/src/components/NavBar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-
+import {Routes,Route,useNavigate} from 'react-router-dom'
+import News from './News';
 const NavBar = () => {
+  const navigate=useNavigate();
+  const navigateToNews =()=>{
+    navigate('/news');
+  };
   return (
     <div>
 <ul className='nav'>
     <li><a href="#">Home</a></li>
-    <li><a href="#">News</a></li>
+    <li><button onClick={navigateToNews}>News</button></li>
     <li><a href="#">Signup</a></li>
     <li className='float'><a href="#">Signin</a></li>
 </ul>
+<Routes>
+<Route path="/news" element={<News/>}/>
+
+</Routes>
     </div>
   )
 }
---
+++ blogapp/src/components/News.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div><h1>1,500 passengers stranded, flights delayed as server issue hits Chennai airport</h1>
+    
+    <p>Over 1,500 passengers were stranded at the airport in Chennai for three hours due to a server problem on Wednesday morning, sources told India Today TV.
+
+    Only international departures were affected, due to disruption in printing boarding passes and baggage handling. The technical issue led to the delay of nine flights destined for Dubai, Paris, Bangkok and Colombo at around 1.30 am.</p></div>
+  )
+}
+
+export default News
